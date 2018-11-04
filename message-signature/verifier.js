3a7e5cd20080f9294ee3fbdb5b86fb0ba1c96d62
Gabriel Givigier <gabriel@givigier.com>
2018-11-03 23:17:00 -0300

    Add tasks of the message signature module

---
+++ message-signature/verifier.js
@@ -0,0 +1,6 @@
+const bitcoinMessage = require('bitcoinjs-message')
+const address = '1HZwkjkeaoZfTSaJxDw6aKkxp45agDiEzN'
+const signature = 'HJLQlDWLyb1Ef8bQKEISzFbDAKctIlaqOpGbrk3YVtRsjmC61lpE5ErkPRUFtDKtx98vHFGUWlFhsh3DiW6N0rE'
+const message = 'This is an example of a signed message.'
+
+console.log(bitcoinMessage.verify(message,address,signature));
